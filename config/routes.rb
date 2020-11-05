75b2fff35c475b40f34586b1fe362a8299293611
cailinkless <cailin.nysff@gmail.com>
2020-11-05 15:31:09 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  Rails.application.routes.draw do
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :coupons, only: [:index, :show, :new, :create]
+  end
 end
