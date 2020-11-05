9a7aca1b91f9d12cb367f628181a305a1e8f9d73
doosemavis <moosedavis2011@gmail.com>
2020-11-05 07:49:59 -0600

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -2,6 +2,19 @@ class CouponsController < ApplicationController
 
     def index 
         @coupons = Coupon.all
+    end
+
+    def show
+        @coupon = Coupon.find_by(id: params[:id])
+    end 
+
+    def new
+        @coupon = Coupon.new
+    end 
+
+    def create
+        @coupon = Coupon.create(store: params[:store])
+        redirect_to coupon
     end 
 
 end 
