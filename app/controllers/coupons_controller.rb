5c4d7da9ea26f6b6968051abbcd0f9ec39e21d32
palepanorama <matthew.keith.thomas@gmail.com>
2020-11-06 13:24:25 -0600

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,18 @@
+class CouponsController < ApplicationController 
+    def index 
+        @coupons = Coupon.all
+    end 
+
+    def show
+        @coupon = Coupon.find_by(id: params[:id])
+    end
+    
+    def new 
+        @coupon = Coupon.new 
+    end 
+
+    def create
+        @coupon = Coupon.create(store: params[:coupon][:store], coupon_code: params[:coupon][:coupon_code])
+        redirect_to coupon_path(@coupon.id)
+    end 
+end 
